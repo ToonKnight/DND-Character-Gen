9aae371dbec990d3c22638071cf6096e64788036
Nick Farrugia <nick.farrugia@altenar.com>
2024-07-02 10:46:45 +0200

    removed files that weren't needed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // app.js
 import AppServer from './AppServer.js';
 
-const port = process.env.PORT || 3000; // Use the environment variable or default to 3000
+const port = process.env.PORT || 3001; // Use the environment variable or default to 3000
 new AppServer(port);
